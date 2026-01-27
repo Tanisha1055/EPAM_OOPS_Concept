efef0eae384322e39fc339bbe539e5c500ae1c4f
Tanisha Bera <tanisha_bera@epam.com>
2026-01-27 11:31:11 +0530

    Added notes for Association

---
+++ src/main/java/org/child1/Association/Main.java
@@ -0,0 +1,17 @@
+package org.child1.Association;
+
+public class Main {
+    public static void main(String[] args) {
+        //Association defines relationship between two classes, i.e. the two classes use .
+        //But here two classes can exists without each other , i.e we don;t have ownership
+        //between the 2 classes(unlike association  that's the difference between those)
+        //Like Doctor and Patients, so both can exist without each other .
+        //(Ex : similar to aggregation example).
+
+        //Inheritance has a 'IS-A' relationship , between patrent and child , as parent is
+        //a part of the child(child is a bigger with extra properties) , so Child 'IS-A'
+        //parent. So Dog 'IS-A' Animal .
+        //(So the parent is created first and the child is created when we write child obj
+        //as will be requiring parent's properties). (So parent exists inside of child) .
+    }
+}
