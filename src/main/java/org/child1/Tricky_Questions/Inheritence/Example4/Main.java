89d72ed4aaab20a9dfc49ddd3fd4449aaafca4be
Tanisha Bera <tanisha_bera@epam.com>
2026-01-30 16:28:55 +0530

    Theory portion related to obj initialisation and others added

--- src/main/java/org/child1/Tricky_Questions/Inheritence/Example4/Main.java
+++ src/main/java/org/child1/Tricky_Questions/Inheritence/Example4/Main.java
@@ -61,3 +61,22 @@ public class Main {
 //Even though the parent constructor is running, the overridden method in the child is invoked
 //due to dynamic binding. However, since the child object is not fully initialized, its fields hold
 //default values. That’s why x is printed as 0.
+
+
+//Java Object Construction & Initialization Rules:
+
+//1. Memory Allocation
+//When you create an object (new Child()), memory is allocated for all fields (including inherited ones),
+//and all fields are set to their default values (0 for int, null for objects, false for boolean, etc.).
+
+//2. Field Initializers and Instance Initializer Blocks
+//Field initializers and instance initializer blocks are executed in the order they appear in the class,
+//after the parent constructor has run.
+
+//3. Constructor Execution Order
+//Parent (superclass) constructor runs first, then the child (subclass) constructor.
+//If you don’t explicitly call super(...) in the child constructor, Java inserts a call to the
+//no-argument parent constructor automatically.
+
+//4. Static Initializers
+//Static fields and static initializer blocks are executed once when the class is first loaded, in the order they appear.
